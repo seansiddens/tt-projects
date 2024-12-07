0fed3f580db9c313a9ccea3e4dfe7878d7569020
Sean Siddens <seansiddens@gmail.com>
2024-12-07 01:04:03 +0000

    Working saxpy test

--- tests/current_test.cpp
+++ tests/current_test.cpp
@@ -9,7 +9,7 @@
 #include "stream.hpp"
 
 TEST(CurrentTests, B16EltwiseSAXPY) {
-    uint32_t count = 1024 * 1;
+    uint32_t count = 1024 * 4;
     auto type = tt::DataFormat::Float16_b;
     uint32_t n_tiles = std::ceil(count / TILE_SIZE);
 
@@ -22,7 +22,7 @@ TEST(CurrentTests, B16EltwiseSAXPY) {
     // max_float, seed); std::vector<uint32_t> generator1_data = create_random_vector_of_bfloat16(TILE_SIZE * n_tiles *
     // datum_size(type), max_float, seed);
     std::vector<uint32_t> generator0_data =
-        create_constant_vector_of_bfloat16(TILE_SIZE * n_tiles * datum_size(type), 3.0F);
+        create_constant_vector_of_bfloat16(TILE_SIZE * n_tiles * datum_size(type), -1.0F);
     std::vector<uint32_t> generator1_data =
         create_constant_vector_of_bfloat16(TILE_SIZE * n_tiles * datum_size(type), 4.0F);
     std::vector<uint32_t> output_data = create_constant_vector_of_bfloat16(TILE_SIZE * n_tiles * 2, 0.0f);
@@ -32,11 +32,11 @@ TEST(CurrentTests, B16EltwiseSAXPY) {
 
     // Define ports and set compute kernel.
     kernel_a.add_input_port("in0", type);
-    // kernel_a.add_input_port("in1", type);
+    kernel_a.add_input_port("in1", type);
     kernel_a.add_output_port("out0", type);
     kernel_a.set_compute_kernel(
         R"(
-        out0 = in0 * 2.0;
+        out0 = in0 * 2.0 + in1;
     )",
         false);
 
@@ -46,9 +46,10 @@ TEST(CurrentTests, B16EltwiseSAXPY) {
     current::Stream sink(output_data, count, type);
 
     // Define connections between streams and kernels.
-    current::Map map({&kernel_a}, {&source0, &sink});
+    auto max_parallelization_factor = 4;
+    current::Map map({&kernel_a}, {&source0, &source1, &sink}, max_parallelization_factor);
     map.add_connection(&source0, &kernel_a, "in0");
-    // map.add_connection(&source1, &kernel_a, "in1");
+    map.add_connection(&source1, &kernel_a, "in1");
     map.add_connection(&kernel_a, "out0", &sink);
 
     // Execute program.
@@ -58,12 +59,18 @@ TEST(CurrentTests, B16EltwiseSAXPY) {
     auto out = map.read_stream(&sink);
     auto out_bf16 = unpack_uint32_vec_into_bfloat16_vec(out);
     EXPECT_EQ(out_bf16.size(), TILE_SIZE * n_tiles);
+    for (size_t i = 0; i < out_bf16.size(); i++) {
+        std::cout << i << ": " << out_bf16[i].to_float() << "\n";
+    }
+    std::cout << "\n";
+
     auto in0 = unpack_uint32_vec_into_bfloat16_vec(generator0_data);
-    // auto in1 = unpack_uint32_vec_into_bfloat16_vec(generator1_data);
+    auto in1 = unpack_uint32_vec_into_bfloat16_vec(generator1_data);
     bool pass = true;
     for (size_t i = 0; i < out_bf16.size(); i++) {
         // Check that out[i] = in0[i] * 2.0 + in1[i]
-        auto expected = bfloat16(in0[i].to_float() * 2.0F);
+        auto expected = bfloat16(in0[i].to_float() * 2.0F + in1[i].to_float());
+        // auto expected = bfloat16(2.0F);
         pass &= is_close(expected.to_float(), out_bf16[i].to_float());
     }
     EXPECT_TRUE(pass);
